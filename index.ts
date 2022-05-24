7f59564215b6643f864dcca6167105e7a212c7ac
wangshaoli <1125550225@qq.com>
2022-05-24 16:25:14 +0800

    得到请求体

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 import * as http from "http";
+import {IncomingMessage, ServerResponse} from "http";
 
 const server = http.createServer();
 
 
-server.on('request', (request, response) => {
-    console.log('有人请求了')
-    response.end('hi')
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
+    const arr = [];
+    request.on('data', (chunk) => {
+        arr.push(chunk);
+    })
+
+    request.on('end', () => {
+        const body = Buffer.concat(arr).toString();
+        console.log(body)
+        response.end('hi')
+    })
 })
 
 
