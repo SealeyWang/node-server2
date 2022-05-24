e558a66858d91f804cf09429c8d855d90b088016
wangshaoli <1125550225@qq.com>
2022-05-24 19:12:37 +0800

    匹配任意文件 & 处理 404

--- index.ts
+++ index.ts
@@ -12,28 +12,31 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     const {method, url: path2, headers} = request
     const obj = url.parse(path2);
     const {pathname, search} = obj;
-    switch (pathname) {
-        case '/index.html':
-            response.setHeader('Content-Type', 'text/html; charset=utf-8');
-            fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
-                if (error) throw error
-                response.end(data.toString())
-            })
-            break
-        case '/style.css':
-            fs.readFile(path.resolve(publicDir, 'style.css'), (error, data) => {
-                if (error) throw error
-                response.setHeader('Content-Type', 'text/css; charset=utf-8')
-                response.end(data.toString())
-            })
-            break
-        case '/main.js':
-            fs.readFile(path.resolve(publicDir, 'main.js'), (error, data) => {
-                if (error) throw error
-                response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
-                response.end(data.toString())
-            })
-            break
+
+    let filename = pathname.substring(1);
+
+    if(filename === '') {
+        filename = 'index.html'
     }
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    fs.readFile(path.resolve(publicDir, filename), (error, data) => {
+        if (error) {
+            if (error.errno === -4058) {
+                response.statusCode = 404
+                fs.readFile(path.resolve(publicDir, '404.html'), (error, data) => {
+                    response.end(data)
+                })
+
+            }else if(error.errno === -4068){
+                response.statusCode = 403
+                response.end('无权访问目录')
+            } else {
+                response.end('服务器繁忙')
+            }
+        } else {
+            response.end(data)
+
+        }
+    })
 })
 server.listen('8888')
