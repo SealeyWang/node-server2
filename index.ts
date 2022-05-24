227a27001f68563b00b3c0762e808b0515530717
wangshaoli <1125550225@qq.com>
2022-05-24 18:08:32 +0800

    处理查询参数

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 import * as http from "http";
 import {IncomingMessage, ServerResponse} from "http";
-import path = require("path");
 import * as fs from "fs";
+import * as path from "path";
+import * as url from "url";
 
 const server = http.createServer();
 
 
 const publicDir = path.resolve(__dirname, 'public')
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const {method, url, headers} = request
-    switch (url) {
+    const {method, url: path2, headers} = request
+    const obj = url.parse(path2);
+    const {pathname, search} = obj;
+    switch (pathname) {
         case '/index.html':
             response.setHeader('Content-Type', 'text/html; charset=utf-8');
             fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
@@ -32,6 +35,5 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
             })
             break
     }
-
 })
 server.listen('8888')
